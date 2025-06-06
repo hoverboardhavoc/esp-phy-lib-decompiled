6c7a330ea51cd4d0eb768a8c391311ab215bb5a1
blobmaster <blobmaster.crux120@passmail.net>
2025-06-06 15:13:47 +0800

    decompile: upstream aa93b8ad95d8a3c646c24a314480e61b52502920 (2025-06-06) -- 3 files changed
    Upstream commit: https://github.com/espressif/esp-phy-lib/commit/aa93b8ad95d8a3c646c24a314480e61b52502920
    
    Upstream subject: support C61 ECO3 test
    
    Archives decompiled: 19

--- decompiled/libbtbb/bt_bb_v2/bt_bb_ble_diag_all.c
+++ decompiled/libbtbb/bt_bb_v2/bt_bb_ble_diag_all.c
@@ -1,8 +1,8 @@
 /*
- * Last changed at upstream commit 0c849b3d31d44c797c2b48f1b9c1607a8c6c4f82
- * https://github.com/espressif/esp-phy-lib/commit/0c849b3d31d44c797c2b48f1b9c1607a8c6c4f82
- * Upstream date: 2024-11-28 14:21:09 +0800
- * Upstream subject: feat(wifi): add phy support for esp32c5 beta5
+ * Last changed at upstream commit aa93b8ad95d8a3c646c24a314480e61b52502920
+ * https://github.com/espressif/esp-phy-lib/commit/aa93b8ad95d8a3c646c24a314480e61b52502920
+ * Upstream date: 2025-06-06 15:13:47 +0800
+ * Upstream subject: support C61 ECO3 test
  * Source: libbtbb -> bt_bb_v2.o -> bt_bb_ble_diag_all
  *
  * (C) Espressif, Apache License 2.0.
@@ -12,22 +12,21 @@
 
 /* WARNING: Globals starting with '_' overlap smaller symbols at the same address */
 
-void bt_bb_ble_diag_all(uint param_1,uint param_2,uint param_3,uint param_4,int param_5,uint param_6
-                       ,int param_7,uint param_8)
+void bt_bb_ble_diag_all(uint param_1,uint param_2,uint param_3,int param_4,uint param_5,int param_6,
+                       uint param_7)
 
 {
   uint in_stack_00000000;
   uint in_stack_00000004;
   
-  _DAT_600a9404 = _DAT_600a9404 & 0xfffffff0 | param_1 & 0xf;
   _DAT_600a210c =
-       (param_6 & 0xf) << 0x18 |
-       (param_4 & 0xf) << 0x10 |
-       (param_3 & 0xf) << 0x14 | (param_2 & 7) << 0xd | _DAT_600a210c & 0x1fff | param_5 << 0x1c;
+       (param_5 & 0xf) << 0x18 |
+       (param_3 & 0xf) << 0x10 |
+       (param_2 & 0xf) << 0x14 | (param_1 & 7) << 0xd | _DAT_600a210c & 0x1fff | param_4 << 0x1c;
   _DAT_600a20ac =
        (in_stack_00000004 & 7) << 0x14 |
        (in_stack_00000000 & 7) << 0x17 |
-       (param_8 & 7) << 0x1a | _DAT_600a20ac & 0xfffff | param_7 << 0x1d;
+       (param_7 & 7) << 0x1a | _DAT_600a20ac & 0xfffff | param_6 << 0x1d;
   return;
 }
 
--- decompiled/librfate/ate_test/rfrx_dcap_cal.c
+++ decompiled/librfate/ate_test/rfrx_dcap_cal.c
@@ -1,8 +1,8 @@
 /*
- * Last changed at upstream commit 772432d2e9e7422159ee3ef01a07fc985ce9466a
- * https://github.com/espressif/esp-phy-lib/commit/772432d2e9e7422159ee3ef01a07fc985ce9466a
- * Upstream date: 2024-08-30 17:42:59 +0800
- * Upstream subject: feat(phy): add phy support for esp32c61
+ * Last changed at upstream commit aa93b8ad95d8a3c646c24a314480e61b52502920
+ * https://github.com/espressif/esp-phy-lib/commit/aa93b8ad95d8a3c646c24a314480e61b52502920
+ * Upstream date: 2025-06-06 15:13:47 +0800
+ * Upstream subject: support C61 ECO3 test
  * Source: librfate -> ate_test.o -> rfrx_dcap_cal
  *
  * (C) Espressif, Apache License 2.0.
@@ -34,9 +34,9 @@ uint rfrx_dcap_cal(void)
     phy_start_tx_tone_step(1,0x80,uVar4,0,0,0);
     ets_delay_us(1);
     phy_iq_est_enable(1,0xfff);
-    if (iVar5 < _DAT_600a0464 >> 9) {
+    if (iVar5 < _DAT_600a046c >> 9) {
       uVar2 = uVar1 & 0xff;
-      iVar5 = _DAT_600a0464 >> 9;
+      iVar5 = _DAT_600a046c >> 9;
     }
     phy_iq_est_disable();
     phy_stop_tx_tone(0);
