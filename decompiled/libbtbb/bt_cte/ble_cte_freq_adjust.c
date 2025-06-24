2aae3364924769d59bbd1782ead61c30dea318a0
blobmaster <blobmaster.crux120@passmail.net>
2025-06-24 14:22:56 +0800

    decompile: upstream ada08d173b90188fc779ad05a75de2bb437b1a06 (2025-06-24) -- 1 files changed
    Upstream commit: https://github.com/espressif/esp-phy-lib/commit/ada08d173b90188fc779ad05a75de2bb437b1a06
    
    Upstream subject: support C61ECO3 sleep test
    
    Archives decompiled: 19

--- decompiled/libbtbb/bt_cte/ble_cte_freq_adjust.c
+++ decompiled/libbtbb/bt_cte/ble_cte_freq_adjust.c
@@ -1,8 +1,8 @@
 /*
- * Last changed at upstream commit 2fb71262baf38dc83591b4191731a0880bfdd3a5
- * https://github.com/espressif/esp-phy-lib/commit/2fb71262baf38dc83591b4191731a0880bfdd3a5
- * Upstream date: 2025-05-15 15:25:17 +0800
- * Upstream subject: update C5ECO2 libphy, for deep sleep and temp track bug
+ * Last changed at upstream commit ada08d173b90188fc779ad05a75de2bb437b1a06
+ * https://github.com/espressif/esp-phy-lib/commit/ada08d173b90188fc779ad05a75de2bb437b1a06
+ * Upstream date: 2025-06-24 14:22:56 +0800
+ * Upstream subject: support C61ECO3 sleep test
  * Source: libbtbb -> bt_cte.o -> ble_cte_freq_adjust
  *
  * (C) Espressif, Apache License 2.0.
@@ -12,7 +12,7 @@
 
 /* WARNING: Globals starting with '_' overlap smaller symbols at the same address */
 
-void ble_cte_freq_adjust(int param_1,int param_2,uint param_3,int param_4,uint param_5)
+void ble_cte_freq_adjust(int param_1,int param_2,uint param_3,int param_4,uint param_5,uint param_6)
 
 {
   if (param_1 == 0) {
@@ -23,6 +23,7 @@ void ble_cte_freq_adjust(int param_1,int param_2,uint param_3,int param_4,uint p
   }
   _DAT_600a215c = (param_3 & 0xff) << 3 | _DAT_600a215c & 0x807 | param_2 << 0xc;
   _DAT_600a21c0 = (param_5 & 0xf) << 8 | _DAT_600a21c0 & 0xff | param_4 << 0xc;
+  _DAT_600a200c = (param_6 & 0xf) << 8 | _DAT_600a200c & 0xfffff0ff;
   return;
 }
 
