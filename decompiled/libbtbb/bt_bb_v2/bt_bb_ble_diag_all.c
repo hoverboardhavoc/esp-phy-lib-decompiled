867446e438d358e936e866411fd703b6af94bf49
blobmaster <blobmaster.crux120@passmail.net>
2025-11-17 14:20:47 +0800

    decompile: upstream fbc304747bc55b40ef7225130fcf87f43b981482 (2025-11-17) -- 2 files changed
    Upstream commit: https://github.com/espressif/esp-phy-lib/commit/fbc304747bc55b40ef7225130fcf87f43b981482
    
    Upstream subject: libphy support C5ECO3 and ECO2 coex, fix C6 track reset
    
    Archives decompiled: 19

--- decompiled/libbtbb/bt_bb_v2/bt_bb_ble_diag_all.c
+++ decompiled/libbtbb/bt_bb_v2/bt_bb_ble_diag_all.c
@@ -1,8 +1,8 @@
 /*
- * Last changed at upstream commit aa93b8ad95d8a3c646c24a314480e61b52502920
- * https://github.com/espressif/esp-phy-lib/commit/aa93b8ad95d8a3c646c24a314480e61b52502920
- * Upstream date: 2025-06-06 15:13:47 +0800
- * Upstream subject: support C61 ECO3 test
+ * Last changed at upstream commit fbc304747bc55b40ef7225130fcf87f43b981482
+ * https://github.com/espressif/esp-phy-lib/commit/fbc304747bc55b40ef7225130fcf87f43b981482
+ * Upstream date: 2025-11-17 14:20:47 +0800
+ * Upstream subject: libphy support C5ECO3 and ECO2 coex, fix C6 track reset
  * Source: libbtbb -> bt_bb_v2.o -> bt_bb_ble_diag_all
  *
  * (C) Espressif, Apache License 2.0.
@@ -13,20 +13,19 @@
 /* WARNING: Globals starting with '_' overlap smaller symbols at the same address */
 
 void bt_bb_ble_diag_all(uint param_1,uint param_2,uint param_3,int param_4,uint param_5,int param_6,
-                       uint param_7)
+                       uint param_7,uint param_8)
 
 {
   uint in_stack_00000000;
-  uint in_stack_00000004;
   
+  _DAT_600a9404 = _DAT_600a9404 & 0xfffffc00 | 0x14e;
   _DAT_600a210c =
        (param_5 & 0xf) << 0x18 |
        (param_3 & 0xf) << 0x10 |
        (param_2 & 0xf) << 0x14 | (param_1 & 7) << 0xd | _DAT_600a210c & 0x1fff | param_4 << 0x1c;
   _DAT_600a20ac =
-       (in_stack_00000004 & 7) << 0x14 |
-       (in_stack_00000000 & 7) << 0x17 |
-       (param_7 & 7) << 0x1a | _DAT_600a20ac & 0xfffff | param_6 << 0x1d;
+       (in_stack_00000000 & 7) << 0x14 |
+       (param_8 & 7) << 0x17 | (param_7 & 7) << 0x1a | _DAT_600a20ac & 0xfffff | param_6 << 0x1d;
   return;
 }
 
--- decompiled/libbttestmode/ble_tx_rx_test/write_source_freq_info.c
+++ decompiled/libbttestmode/ble_tx_rx_test/write_source_freq_info.c
@@ -1,8 +1,8 @@
 /*
- * Last changed at upstream commit 1d4cd3aafa244a0adf5891f058b3519bc970d644
- * https://github.com/espressif/esp-phy-lib/commit/1d4cd3aafa244a0adf5891f058b3519bc970d644
- * Upstream date: 2025-08-01 19:50:02 +0800
- * Upstream subject: 1. fix C5ECO2 signaling test power 2. fix C5ECO2/C6ECO3/H2ECO5 coex problem
+ * Last changed at upstream commit fbc304747bc55b40ef7225130fcf87f43b981482
+ * https://github.com/espressif/esp-phy-lib/commit/fbc304747bc55b40ef7225130fcf87f43b981482
+ * Upstream date: 2025-11-17 14:20:47 +0800
+ * Upstream subject: libphy support C5ECO3 and ECO2 coex, fix C6 track reset
  * Source: libbttestmode -> ble_tx_rx_test.o -> write_source_freq_info
  *
  * (C) Espressif, Apache License 2.0.
@@ -16,12 +16,12 @@ void write_source_freq_info(int param_1,undefined4 *param_2,undefined1 param_3)
 
 {
   if ((ble_freq_est != '\0') && (param_1 != 0)) {
-    _DAT_0001102e = ble_freq_err;
-    DAT_0001102c = (undefined1)*param_2;
-    DAT_00011031 = (undefined1)param_1;
-    DAT_0001102d = (undefined1)param_2[1];
+    _DAT_0001103a = ble_freq_err;
+    DAT_00011038 = (undefined1)*param_2;
+    DAT_0001103d = (undefined1)param_1;
+    DAT_00011039 = (undefined1)param_2[1];
     _DAT_600a1830 = &ble_radio_tx_buffer;
-    DAT_00011030 = param_3;
+    DAT_0001103c = param_3;
   }
   return;
 }
