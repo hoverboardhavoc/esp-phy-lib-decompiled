9eb1a00bbce247b6e1bf579dce96606ba021654d
blobmaster <blobmaster.crux120@passmail.net>
2026-05-15 06:58:23 +0000

    decompile: upstream e37ecdc9c1e93fefda5b1a99f09bd979046d0720 (2026-05-15) -- 1 files changed
    Upstream commit: https://github.com/espressif/esp-phy-lib/commit/e37ecdc9c1e93fefda5b1a99f09bd979046d0720
    
    Upstream subject: support h21 eco2 test
    
    Archives decompiled: 28

---
+++ decompiled/libbtbb/bt_bb_v2/bt_bb_tx_cca_fifo_read_all.c
@@ -0,0 +1,20 @@
+/*
+ * Last changed at upstream commit e37ecdc9c1e93fefda5b1a99f09bd979046d0720
+ * https://github.com/espressif/esp-phy-lib/commit/e37ecdc9c1e93fefda5b1a99f09bd979046d0720
+ * Upstream date: 2026-05-15 06:58:23 +0000
+ * Upstream subject: support h21 eco2 test
+ * Source: libbtbb -> bt_bb_v2.o -> bt_bb_tx_cca_fifo_read_all
+ *
+ * (C) Espressif, Apache License 2.0.
+ * Derivative work (this file): mechanical decompile via Ghidra (NSA, Apache 2.0).
+ * Decompiler output may be incomplete or differ from original semantics.
+ */
+
+/* WARNING: Globals starting with '_' overlap smaller symbols at the same address */
+
+undefined4 bt_bb_tx_cca_fifo_read_all(void)
+
+{
+  return _DAT_600a20d8;
+}
+
